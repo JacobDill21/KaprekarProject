9c05b2209d221400dd5b3980890e1d49cf2abb38
JacobDill <44781182+JacobDill@users.noreply.github.com>
2019-09-11 17:52:23 -0500

    Update kaprekarDemo.c
    Changed the arguments back to how they started. I was confused before

--- c_version/kaprekarDemo.c
+++ c_version/kaprekarDemo.c
@@ -6,11 +6,11 @@
 int main(int argc, char **argv) {
 
   if(argc != 2) {
-    fprintf(stderr, "Usage: %s n\n", argv[1]);
+    fprintf(stderr, "Usage: %s n\n", argv[0]);
     exit(1);
   }
 
-  int n = atoi(argv[2]);
+  int n = atoi(argv[1]);
 
   if(isKaprekar(n)) {
     printf("%d is a Kaprekar Number!\n", n);
